f92756b155bb3395d6fd7dd6f26beb26d6cb5c8c
果 <>
2025-11-08 09:32:37 +0800

    first commit

---
+++ howtouse.md
@@ -0,0 +1,23 @@
+# Potato Lang 1.0.0
+By aPpLegUo
+
+### i 
+get input
+### out "text";
+out "text"
+#### if you write soo many space begin text, the parser'll be skip that space.
+#### "out" or "OUT" both can output "text"
+### out `;
+out input text
+#### if you haven't write something, the parser'll be output "have not input".
+### out n;
+"\n"
+### POT
+begin of annotation
+### ATO
+end of annotation
+#### just that
+
+### Hello World
+#### out "hello world"n; out "what's your name? "; i out "hello, "`"!"; POT hello world! ATO
+
---
+++ main.cpp
@@ -0,0 +1,43 @@
+// PotatoLang 1.0.0 Parser
+// By aPpLegUo
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    printf("Potato Programming Language Parser\n");
+    printf("Enter code (max 20 characters): ");
+
+    char code[32767];
+    bool isAnn = false;
+    bool isStr = false;
+    char input[32767] = "have not input";
+    cin.getline(code, 32767);
+    for(int i = 0; i < strlen(code); i++) {
+        if(code[i] == 'P' && code[i + 1] == 'O' && code[i + 2] == 'T') isAnn = true;
+        if(code[i] == 'A' && code[i + 1] == 'T' && code[i + 2] == 'O') isAnn = false;
+        if(!isAnn) {
+            if (code[i] == 'O' && code[i + 1] == 'U' && code[i + 2] == 'T' || code[i] == 'o' && code[i + 1] == 'u' && code[i + 2] == 't') {
+                for (int j = i + 3; j < strlen(code); j++) {
+                    if (code[j] == ';') break;
+                    if (code[j] == '"') {
+                        isStr = !isStr;
+                    }
+                    else if (code[j] == '`') {
+                        for(int k = 0; k < strlen(input); k++){
+                            printf("%c", input[k]);
+                        }
+                    }
+                    else if (isStr) printf("%c", code[j]);
+                    else if (code[j] == 'n') printf("\n");
+                }
+            }
+            else if (code[i] == 'i') {
+                scanf("%32766s", input);
+            }
+        }
+    }
+
+    printf("\nParsing completed.\n");
+    return 0;
+}
